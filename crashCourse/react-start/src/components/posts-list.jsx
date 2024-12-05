3fc53c20bef5ad5a9461b2b9fb0d27a068e28179
Leah <leaheliz18@gmail.com>
2024-12-04 20:43:54 -0500

    Modified react-start

--- crashCourse/react-start/src/components/posts-list.jsx
+++ crashCourse/react-start/src/components/posts-list.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import Post from "./post";
 import NewPost from "./new-post";
+import Modal from './modal'
 import classes from "./posts-list.module.css";
 
 function PostsList() {
+  const [enteredBody, setBody] = useState("");
+  const [enteredAuthor, setAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setBody(event.target.value);
+  }
+  function authorChangeHandler(event) {
+    setAuthor(event.target.value);
+  }
   return (
     <>
-      <NewPost />
+      <Modal>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandler}
+        />
+      </Modal>
       <ul className={classes.posts}>
-        <Post author="Leah" body="I love boba" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Seah" body="I love coke" />
       </ul>
     </>
